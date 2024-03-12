a368230b8a727261ad2bc5d631204ae205d4d2ce
Stanley <stanleybrenner@gmail.com>
2024-03-12 11:02:29 -0300

    Using localhost for tests

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
 import { db } from "./db";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: true }));
 
 interface Requisition {
